cd00527dc6e7ed03d548dd4dcabb5cc0ae31ef62
Lars Føien <larsmustars@gmail.com>
2021-09-30 13:43:45 +0200

    eidt

--- Exercise 1 (more programming problems)/1-4.py
+++ Exercise 1 (more programming problems)/1-4.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  5 14:49:31 2021
-
-@author: larsm
-"""
 print("Enter the appropriate for your car","\n")
 gal = float(input("Number of gallons in the tank: ")) #20gal = 80liter
 effi = float(input("The fuel efficiency in miles per gallon: ")) #20
